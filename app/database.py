fff1ca99219897ece59cddcde2644cdebd8491b0
500ping <ndt512x@gmail.com>
2023-03-27 10:39:10 +0700

    change alembic to mysql

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('sqlite:///dictionary.db')
+# engine = create_engine('sqlite:///dictionary.db')
+engine = create_engine(
+    'mysql+pymysql://root:example@localhost:3306/dictionary')
 
 
 def create_session():
